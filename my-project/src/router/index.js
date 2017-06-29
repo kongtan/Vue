3229fb842d194982f1bb1d89d2ae31d3cf6ddf94
陆智勇 <lzy12611@ly.com>
2017-06-29 17:48:39 +0800

    1

---
+++ my-project/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import test from '@/views/test'
+
+Vue.use(Router)
+
+export default new Router({
+	linkActiveClass:'link',
+	routes: [
+	{
+		path:'/',
+		redirect:'/test'
+	},
+	{
+		path: '/Hello',
+		name: 'Hello',
+		component: Hello
+	},
+	{
+		path: '/test',
+		name: 'test',
+		component: test
+	}
+	]
+})
